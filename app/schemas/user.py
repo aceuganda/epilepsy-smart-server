5471ec495bf5aa9bdae3d7f554a989d9a7c7ebb5
Ssemakula Charles Derrick <51287876+Schade9@users.noreply.github.com>
2022-11-02 18:06:45 +0300

    Fx register (#10)
    * Added fields to user register
    
    * Rectified routes
    
    * Added profile picture to user model
    
    * upgrade of db columns
    
    * editted api_docs with profile image
    
    * changed profile_picture to profileImage
    
    * added profile image to user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,6 +6,14 @@ class UserSchema(Schema):
     id = fields.Integer(dump_only=True)
     email = fields.Email()
     username = fields.String()
+    age = fields.Integer()
+    gender = fields.String()
+    age_of_onset = fields.Integer()
+    seizure_type = fields.String()
+    caregiver_name = fields.String()
+    caregiver_contact = fields.String()
+    institution = fields.String()
+    profileImage = fields.String()
     password = fields.String(load_only=True)
 
 
