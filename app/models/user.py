a02470db4fa965d43894cb82b6bf29c6fa7aaedc
Schade9 <s.charlesderrick9@gmail.com>
2022-11-07 14:50:47 +0300

    fix profile image blob storage

--- app/models/user.py
+++ app/models/user.py
@@ -23,7 +23,7 @@ class User(RootModel):
     caregiver_name = db.Column(db.String(256), nullable=True)
     caregiver_contact = db.Column(db.String(256), nullable=True)
     institution = db.Column(db.String(256), nullable=True)
-    profileImage = db.Column(db.LargeBinary(), nullable=True)
+    profileImage = db.Column(db.Text, nullable=True)
     password = db.Column(db.String(256), nullable=False, default="")
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     seizures = db.relationship('Seizure', backref='user', lazy=True)
