3b6d1c270be15761edddd227457c0b51f37a2619
Mutegeki Henry <hmutegeki@Henrys-MacBook-Pro.local>
2023-06-08 11:25:12 +0300

    add missing configs and imports

--- config/config.py
+++ config/config.py
@@ -6,7 +6,24 @@ class Base:
     """ base config """
     # main
     SECRET_KEY = os.getenv("FLASK_APP_SECRET")
+    PASSWORD_SALT = os.getenv("FLASK_APP_SALT")
+    VERIFICATION_SALT = os.getenv("FLASK_VERIFY_SALT")
 
+    # mail settings
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = os.getenv("APP_MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("APP_MAIL_PASSWORD")
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = "epilepsy.smartapp@gmail.com"
+    
+    # EXCEPTIONS
+    PROPAGATE_EXCEPTIONS = True
 
 class Development(Base):
     """ development config """
